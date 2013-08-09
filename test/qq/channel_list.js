a494346b3ba7c4d6a08ab2040b043fff76c65e74
wxnet2013 <wxnet2013@gmail.com>
2013-08-09 15:34:08 +0800

    dddd

--- test/qq/channel_list.js
+++ test/qq/channel_list.js
@@ -581,6 +581,7 @@ le.m.loadMore= {
     _pageshow: function(e){
         //bfcache 浏览器返回 缓存
         if(e.persisted) {
+            alert(this.__scroll);
             this._win.on('scroll',this.__scroll);   
         }
     },
